bbd8803557f7560e41082bb58b28956c8f7c1d80
Unnikrishnan P V <unnimonpv@gmail.com>
2019-04-17 13:22:06 +0530

    Add files via upload

---
+++ 1,2.m
@@ -0,0 +1,10 @@
+%documentationMattersSoDoesNaming
+function h = circle()
+hold on
+th = 0:pi/50:2*pi;
+xunit = 3 * cos(th) + 1;
+yunit = 3 * sin(th) + 2;
+h = plot(xunit, yunit), grid on, axis equal
+hold off
+
+%CTAPCCECNNPHTICNCTBNCCRDCCBR_ _ _RBCDSAI
